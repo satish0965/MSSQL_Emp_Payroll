ed8f7d38e0e540321b6dae030878f7534a598aad
Satish Patil <satish.patil7017@gmail.com>
2022-09-13 08:09:37 +0530

    [Satish's] Added Create Table for Employee Payroll

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -2,4 +2,7 @@
 create database payroll_service;
 use payroll_service;
 
+--UC2--Create Table employee_payroll
+create table employee_payroll(Id int identity (1,1) primary key,Name varchar(200),Salary bigint ,StartDate date)
+
 
