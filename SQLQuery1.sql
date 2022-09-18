78959c539ea27ecfb769aa2a57c38b87602b0a60
Satish Patil <satish.patil7017@gmail.com>
2022-09-18 10:13:53 +0530

    [Satish] Added Find Salary And Joining Date

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -16,4 +16,7 @@ insert into employee_payroll (Name, Salary, StartDate) values
 --UC4-- Retrieve employee_payroll data
 select * from employee_payroll;
 
+--UC5-- Retrieve salary of particular employee and particular date range
+select salary from employee_payroll where Name = 'Pritish';
+select * from employee_payroll where StartDate between cast ('2018-01-01' as date) and GETDATE();
 
